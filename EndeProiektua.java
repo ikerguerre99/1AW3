d32c7bd1df8834853dfcd8025dcb6146fbcc6122
ikerguerre99 <57668500+ikerguerre99@users.noreply.github.com>
2019-11-12 13:43:26 +0100

    Add files via upload

---
+++ EndeProiektua.java
@@ -0,0 +1,19 @@
+package Ende;
+
+public class EndeProiektua {
+
+	public static void main(String[] args) {
+		String[] izenak = {"Julen", "Markel", "Igor", "Galder"};
+		int[] adina = {18,19,20,21};
+		
+		System.out.println("kaixo");
+		for(int i=0; i < adina.length; i++){
+
+			System.out.print(izenak[i]);
+			System.out.println(" " + adina[i]);
+		}
+		
+
+	}
+
+}
